7a61083e02a2f16e2f666ac17f97acdf586db65e
Augusto Lopes <augustolfp@gmail.com>
2022-07-03 15:25:09 -0300

    Configures context to share token and user data

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,18 @@ import '../assets/styles/reset.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./LoginScreen/LoginScreen";
 import React from 'react';
+import UserContext from "../contexts/UserContext";
 
 export default function App() {
+    const [token, setToken] = React.useState({});
+    const [userData, setUserData] = React.useState({});
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<LoginScreen />} />
-            </Routes>
-        </BrowserRouter>
+        <UserContext.Provider value={{token, setToken, userData, setUserData}}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LoginScreen />} />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 }
--- src/components/LoginScreen/LoginScreen.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function LoginScreen() {
-    return(
-        <h1>Login screen test</h1>
-    );
-}
