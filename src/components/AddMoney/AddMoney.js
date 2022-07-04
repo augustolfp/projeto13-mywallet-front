e51b55c467ef3736ff2205e10e7dc9cce0120361
Augusto Lopes <augustolfp@gmail.com>
2022-07-04 15:13:25 -0300

    Styled add money  screen

--- src/components/AddMoney/AddMoney.js
+++ src/components/AddMoney/AddMoney.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import UserContext from "../../contexts/UserContext";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import styled from 'styled-components';
 
 export default function AddMoney() {
     const navigate = useNavigate();
@@ -30,12 +31,67 @@ export default function AddMoney() {
     }
 
     return(
-        <>
-            <form onSubmit={handleTransaction}>
-                <input type="text" name="description" onChange={e => setDescription(e.target.value)} required />
-                <input type="number" name="value" onChange={e => setValue(e.target.value)} required />
+        <Container>
+            <TitleBox>
+                Nova entrada
+            </TitleBox>
+            <Form onSubmit={handleTransaction}>
+                <input type="number" name="value" placeholder="Valor" onChange={e => setValue(e.target.value)} required />
+                <input type="text" name="description" placeholder="Descrição" onChange={e => setDescription(e.target.value)} required />
                 <button type="submit">Salvar entrada</button>
-            </form>
-        </>
+            </Form>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #8C11BE;
+
+    input {
+        box-sizing: border-box;
+        height: 46px;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        font-family: 'Raleway', sans-serif;
+        font-size: 20px;
+        margin: 3px 0;
+        padding: 0 6px;
+        
+        ::placeholder {
+            color: black;
+        }
+    }
+
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+
+    button {
+        height: 46px;
+        border-style: none;
+        border-radius: 4px;
+        background-color: #A328D6;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 700;
+        font-size:20px;
+        color:white;
+        margin: 3px 0 18px 0;
+    }
+`
+
+const TitleBox = styled.div`
+    margin: 25px;
+    color: white;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 700;
+    font-size: 26px;
+`
