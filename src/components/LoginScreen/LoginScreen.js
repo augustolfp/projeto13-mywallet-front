f5211ee0abf4884b71fec7007a59f5abdac525c4
Augusto Lopes <augustolfp@gmail.com>
2022-07-04 14:24:04 -0300

    Styled sign-in and sign-up screens

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import UserContext from "../../contexts/UserContext";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import styled from 'styled-components';
 
 
 
@@ -38,13 +39,73 @@ export default function LoginScreen() {
     }
 
     return(
-        <>
-            <form onSubmit={handleLogin}>
-                <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" required />
-                <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="senha" required />
-                <button type="submit">ENTRAR</button>
-            </form>
-            <Link to="/cadastro">Não tem uma conta? Cadastre-se!</Link>
-        </>
+        <Container>
+            <Form onSubmit={handleLogin}>
+                <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" required />
+                <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha" required />
+                <button type="submit">Entrar</button>
+            </Form>
+            <Link to="/cadastro">Primeira vez? Cadastre-se!</Link>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #8C11BE;
+
+    input {
+        box-sizing: border-box;
+        height: 46px;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        font-family: 'Raleway', sans-serif;
+        font-size: 20px;
+        margin: 3px 0;
+        padding: 0 6px;
+        
+        ::placeholder {
+            color: black;
+        }
+    }
+
+    a {
+            font-family: 'Raleway', sans-serif;
+            text-decoration: none;
+            font-weight: 700;
+            font-size: 15px;
+
+        }
+
+        a:link {
+            color: white;
+        }
+
+        a:visited {
+            color: white;
+        }
+
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+
+    button {
+        height: 46px;
+        border-style: none;
+        border-radius: 4px;
+        background-color: #A328D6;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 700;
+        font-size:20px;
+        color:white;
+        margin: 3px 0 18px 0;
+    }
+`
--- src/components/RegisterScreen/RegisterScreen.js
+++ src/components/RegisterScreen/RegisterScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import UserContext from "../../contexts/UserContext";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import styled from 'styled-components';
 
 
 export default function RegisterScreen() {
@@ -34,16 +35,76 @@ export default function RegisterScreen() {
 
 
     return(
-        <>
-            <form onSubmit={handleRegister}>
-                <input type="text" name="name" placeholder="nome" onChange={e => setName(e.target.value)} required />
-                <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" required />
-                <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="senha" required />
-                <input type="password" name="repeat password" value={repeat_password} onChange={e => setRepeat_password(e.target.value)} placeholder="repita a senha" required />
+        <Container>
+            <Form onSubmit={handleRegister}>
+                <input type="text" name="name" placeholder="Nome" onChange={e => setName(e.target.value)} required />
+                <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" required />
+                <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha" required />
+                <input type="password" name="repeat password" value={repeat_password} onChange={e => setRepeat_password(e.target.value)} placeholder="Confirme a senha" required />
 
-                <button type="submit">CADASTRAR</button>
-            </form>
-            <Link to="/">Já tem uma conta? Faça login!</Link>
-        </>
+                <button type="submit">Cadastrar</button>
+            </Form>
+            <Link to="/">Já tem uma conta? Entre agora!</Link>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #8C11BE;
+
+    input {
+        box-sizing: border-box;
+        height: 46px;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        font-family: 'Raleway', sans-serif;
+        font-size: 20px;
+        margin: 3px 0;
+        padding: 0 6px;
+        
+        ::placeholder {
+            color: black;
+        }
+    }
+
+    a {
+            font-family: 'Raleway', sans-serif;
+            text-decoration: none;
+            font-weight: 700;
+            font-size: 15px;
+
+        }
+
+        a:link {
+            color: white;
+        }
+
+        a:visited {
+            color: white;
+        }
+
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+
+    button {
+        height: 46px;
+        border-style: none;
+        border-radius: 4px;
+        background-color: #A328D6;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 700;
+        font-size:20px;
+        color:white;
+        margin: 3px 0 18px 0;
+    }
+`
